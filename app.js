c4ee5e6376e21f1e4fadb54d32d6f137f308fbf3
Jake White <jwhite52@gmail.com>
2020-05-13 10:29:38 -0400

    Add jest scaffolding for testing

---
+++ app.js
@@ -0,0 +1,16 @@
+// seperate app.js and server.js for testing purposes
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Enables incoming requests to be decoded and parsed as json objects
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ encode:true }));
+
+app.get("/", (req, res) => {
+  res.status(200).send("Server can serve data");
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,16 +1,4 @@
-// ./node_modules/nodemon/bin/nodemon.js src/app.js
-const express = require("express");
-const bodyParser = require("body-parser");
-
-const app = express();
-
-// Enables incoming requests to be decoded and parsed as json objects
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ encode:true }));
-
-app.get("/", (req, res) => {
-  res.json({ "message": "Server can respond"});
-});
+const app = require('./app.js');
 
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
