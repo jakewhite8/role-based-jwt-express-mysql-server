7c43787e05c4d2d5807f615721e8e92917758c87
Jake White <jwhite52@gmail.com>
2020-05-15 10:46:04 -0400

    Re-factor model module

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,15 @@
-const connection = require('./db.js');
-const user = require('./user.model.js');
+const mysql = require('mysql2');
+const dbConfig = require('../config/db.config.js');
+
+const connection = mysql.createPool({
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB,
+  port: dbConfig.PORT,
+});
+
+const user = require('./user.model.js')(connection);
 
 module.exports = {
   connection,
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,41 +1,41 @@
-const mysql = require('./db.js');
+module.exports = (connection) => {
+  const User = function (user) {
+    this.username = user.username;
+    this.email = user.email;
+    this.password = user.password;
+  };
 
-const User = function (user) {
-  this.username = user.username;
-  this.email = user.email;
-  this.password = user.password;
+  User.create = (newUser, result) => {
+    // execute() may be the better then using query()
+    connection.query('INSERT INTO users SET ?', newUser, (err, res) => {
+      if (err) {
+        console.error(err);
+        result(err, null);
+        return;
+      }
+
+      console.log(`Create user: ${JSON.stringify({ id: res.insertId, ...newUser })}`);
+      result(null, { id: res.insertId, ...newUser });
+    });
+  };
+
+  User.findOne = (email, result) => {
+    connection.query('SELECT * FROM users WHERE email = ?', email, (err, res) => {
+      if (err) {
+        console.log('Error: ', err);
+        result(err, null);
+        return;
+      }
+
+      if (res.length) {
+        console.log('found user: ', res[0]);
+        result(null, res[0]);
+        return;
+      }
+
+      result({ kind: 'not found' }, null);
+    });
+  };
+
+  return User;
 };
-
-User.create = (newUser, result) => {
-  // execute() may be the better then using query()
-  mysql.query('INSERT INTO users SET ?', newUser, (err, res) => {
-    if (err) {
-      console.error(err);
-      result(err, null);
-      return;
-    }
-
-    console.log(`Create user: ${JSON.stringify({ id: res.insertId, ...newUser })}`);
-    result(null, { id: res.insertId, ...newUser });
-  });
-};
-
-User.findOne = (email, result) => {
-  mysql.query('SELECT * FROM users WHERE email = ?', email, (err, res) => {
-    if (err) {
-      console.log('Error: ', err);
-      result(err, null);
-      return;
-    }
-
-    if (res.length) {
-      console.log('found user: ', res[0]);
-      result(null, res[0]);
-      return;
-    }
-
-    result({ kind: 'not found' }, null);
-  });
-};
-
-module.exports = User;
